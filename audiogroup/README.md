08d037c155732d7e3449fc77cd9f700bb1d6b726
TheCloneMaster <the.clone.master@gmail.com>
2021-11-09 13:26:56 -0600

    Initial commit Audiogroup modifications

---
+++ audiogroup/README.md
@@ -0,0 +1,11 @@
+Module to initialize a db:
+--------------------------
+
+This module contains base setup for Audiogroup
+
+Repositories needed for this module:
+====================================
+
+See oca_dependencies.txt file
+
+Just declare all this repositories on 13.0 branches in your addons-path and install "rroma" module.
---
+++ audiogroup/__manifest__.py
@@ -0,0 +1,29 @@
+# License AGPL-3 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Instance creator for AudioGroup',
+    'summary': '''
+    Instance creator for rroma. This is the app.
+    ''',
+    'author': 'Prointec',
+    'license': 'AGPL-3',
+    'category': 'Installer',
+    'version': '14.0.1.0.0',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        # views
+        #'views/account_move.xml',
+        # reports
+        'reports/purchase_quotation_templates.xml',
+        'reports/purchase_order_templates.xml',
+        #'reports/sale_report_templates.xml',
+        #'reports/report_invoice.xml',
+        #'reports/report_payment_receipt_templates.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
