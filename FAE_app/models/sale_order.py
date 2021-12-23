5fcc8e0b2cbe5bd09e6271c6915458ba149cdeee
eperazaco <eperaza@prointeccr.com>
2021-12-23 11:49:19 -0600

    setea el tipo comprobante de la factura

--- FAE_app/models/sale_order.py
+++ FAE_app/models/sale_order.py
@@ -19,8 +19,18 @@ class SaleOrderInherit(models.Model):
     def _prepare_invoice(self):
         vals = super(SaleOrderInherit, self)._prepare_invoice()
         if vals:
+            if self.partner_id.x_foreign_partner:
+                document_type = 'FEE'
+            elif self.partner_id.vat:
+                if ((self.partner_id.country_id and self.partner_id.country_id.code != 'CR')
+                    or (self.partner_id.x_identification_type_id and self.partner_id.x_identification_type_id.code == '05')):
+                    document_type = 'TE'
+                else:
+                    document_type = 'FE'
+            else:
+                document_type = 'TE'
             vals['x_economic_activity_id'] = self.x_economic_activity_id
-            vals['x_document_type'] = 'FE'
+            vals['x_document_type'] = document_type
         return vals 
 
     @api.onchange('partner_id', 'company_id')
